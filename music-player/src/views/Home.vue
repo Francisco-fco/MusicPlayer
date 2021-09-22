d0272dd1f4e762f94102f0f431ca442854097d14
FCO <franne_@hotmail.com>
2021-09-22 11:04:48 +0200

    quick fix

--- music-player/src/views/Home.vue
+++ music-player/src/views/Home.vue
@@ -1,23 +1,15 @@
 <template>
   <div class="container">
     <SearchBar />
-    <br>
-    <SearchArtist />
-    <br>
-    <SearchSong />
     <div class="result-container"></div>
   </div>
 </template>
 
 <script>
 import SearchBar from "../components/SearchBar.vue";
-import SearchArtist from "../components/SearchArtist.vue";
-import SearchSong from "../components/SearchSong.vue";
 export default {
   components: {
     SearchBar,
-    SearchArtist,
-    SearchSong
   },
 };
 </script>
